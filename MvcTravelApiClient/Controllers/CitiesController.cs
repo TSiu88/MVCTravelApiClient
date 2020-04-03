6b7abe083c55ca4e81f33afff147c5545734d2d5
Tiffany Siu <tsiu88@gmail.com>
2020-04-03 05:11:19 -0700

    Fix to show all cities and countries in MVC API Call

--- MvcTravelApiClient/Controllers/CitiesController.cs
+++ MvcTravelApiClient/Controllers/CitiesController.cs
@@ -1,17 +1,17 @@
-// using System.Collections.Generic;
-// using System.Linq;
-// using Microsoft.AspNetCore.Mvc;
-// using MvcTravelApiCall.Models;
-// using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MvcTravelApiCall.Models;
+using Microsoft.EntityFrameworkCore;
 
-// namespace MvcTravelApiCall.Controllers
-// {
-//   public class CitiesController : Controller
-//   {
-//     public IActionResult Index()
-//     {
-//       var allCities = City.GetCities();
-//       return View(allCities);
-//     }
-//   }
-// }
+namespace MvcTravelApiCall.Controllers
+{
+  public class CitiesController : Controller
+  {
+    public IActionResult Index()
+    {
+      var allCities = City.GetCities();
+      return View(allCities);
+    }
+  }
+}
